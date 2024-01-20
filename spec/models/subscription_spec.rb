7f61ff537a34faca26bb43e1798457378d95e49b
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-19 20:34:45 -0600

    TEST- model testing of subscriptions and validations of a subscriptions columns. All testing passes.

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do 
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+  end 
+
+  describe 'enum' do
+    it { should define_enum_for(:status).with_values(inactive: 0, active: 1) }
+  end
 end
