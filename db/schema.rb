dadd0f702e5a1d34dcb377459fdf80df7fd2bd36
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-19 19:57:50 -0600

    Customer data base coulmns migrated, validations added to Customer model, moving on to testing.

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_20_014628) do
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "address"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
