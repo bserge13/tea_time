63b7d43d647da639538ef869319a6cf23e95bf01
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-19 21:11:06 -0600

    Seeded data for teas in the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# TEAS 
+tea_1 = Tea.create!(title: "Earl Grey", description: "Combines black tea with bergamot orange extract", temperature: 90, brew_time: 4)
+tea_2 = Tea.create!(title: "Decaf Tea", description: "Tea without caffine", temperature: 90, brew_time: 5)
+tea_3 = Tea.create!(title: "Green Tea", description: "Good for lifting your spirits", temperature: 90, brew_time: 4)
+tea_4 = Tea.create!(title: "Yellow Tea", description: "A rare variety of tea, similar processing as Green Tea", temperature: 88, brew_time: 5)
+tea_5 = Tea.create!(title: "Herbal", description: "Good aall around for grounding the nerves and boosting the spirits", temperature: 80, brew_time: 7)
+tea_6 = Tea.create!(title: "Chamomile", description: "Common herbal tea ingredients include herbs and spices like chamomile, peppermint, ginger, lavender, and more", temperature: 89, brew_time: 3)
+tea_7 = Tea.create!(title: "Assam", description: "Loose Leaf - Black Tea", temperature: 87, brew_time: 4)
+tea_8 = Tea.create!(title: "Hibiscus Tea", description: "Is derived from the hibiscus plant", temperature: 88, brew_time: 5)
+tea_9 = Tea.create!(title: "Mint Tea", description: "Herbal mint tea assortment", temperature: 93, brew_time: 7)
+tea_10 = Tea.create!(title: "Breakfast Tea", description: "Good with any meal, but we suggest first thing to get you going", temperature: 92, brew_time: 2)
+tea_11 = Tea.create!(title: "Ginger Peach", description: "A taste of sweet and spice, this tea makes a great pair", temperature: 95, brew_time: 3)
+tea_12 = Tea.create!(title: "Citrus Tea", description: "Nice floral notes to balance the fresh citrus notes", temperature: 90, brew_time: 6)
+tea_13 = Tea.create!(title: "Honey Ginger Tea", description: "SMooth and warm- best for any day, but especially a cold one", temperature: 91, brew_time: 4)
+tea_14 = Tea.create!(title: "Floral Tea", description: "Hibiscus notes coupled with rose and lavander", temperature: 92, brew_time: 7)
+tea_15 = Tea.create!(title: "Chai", description: "Though it's technically a black tea, chai tea has a specific flavor profile and preparation process that differentiates it", temperature: 91, brew_time: 3)
+
+# SUBSCRIPTIONS 
+
